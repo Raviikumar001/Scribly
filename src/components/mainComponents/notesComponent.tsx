1f0860c2f231d61f92a9736a849c1038bee3478a
ravi <samcollins9899@gmail.com>
2023-08-29 09:44:43 +0530

    added input

--- src/components/mainComponents/notesComponent.tsx
+++ src/components/mainComponents/notesComponent.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import  {SerchIcon }from '../SvgFiles'
+import {useState} from 'react'
+import  {SerchIcon, CrossArrow2 }from '../SvgFiles'
 const NotesComponent = () => {
+  const [inputText, setInputText] = useState("")
+
+  const Change = (event:React.ChangeEvent<HTMLInputElement>)=>{
+    setInputText(event.target.value)
+  }
+
+  const onMouseClick = ()=>{
+    setInputText("")
+  }
+
+
   return (
     <div>
       <div className='flex  justify-center'>
         <button className='pl-3 pr-2'>
         <SerchIcon />
         </button>
-        <input type="text" placeholder='Search all notes' className='w-[80%] h-9 ' />
+        
+        <input value={inputText} onChange={Change} type="text" placeholder='Search all notes' className='w-full h-9 border border-transparent focus:outline-none' />
+        {inputText && ( <button onClick={onMouseClick}><CrossArrow2 /></button>  ) }
       </div>
 
     </div>
