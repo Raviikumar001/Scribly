3601ba158565455f03b8b1800e6ff230b8a763c4
ravi <samcollins9899@gmail.com>
2023-09-02 22:20:01 +0530

    added last fix for verify

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -1,9 +1,9 @@
-import Register from "./auth/Register";
-import Features from "./Features";
-import Header from "./Header";
-import LandingPage from "./LandingPage";
-import Footer from "./Footer";
-import Login from "./auth/Login";
+// import Register from "./auth/Register";
+// import Features from "./Features";
+// import Header from "./Header";
+// import LandingPage from "./LandingPage";
+// import Footer from "./Footer";
+// // import Login from "./auth/Login";
 
-export
-{Register,Features,Header,LandingPage,Footer,Login}
+// export
+// {Register,Features,Header,LandingPage,Footer,Login}
