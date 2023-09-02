3bb25e7253e45f3354c1bc692163e7f20ae9c276
ravi <samcollins9899@gmail.com>
2023-09-02 22:21:33 +0530

    added last verify commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,15 @@
-import {Header,LandingPage,Footer} from './pages'
+// import {Header,LandingPage,Footer} from './pages'
 
 
 
-const Home = () => {
-  return (
-    <div className='min-h-screen bg_gradient'>
-        <Header />
-        <LandingPage />
-        <Footer />
-    </div>
-  )
-}
+// const Home = () => {
+//   return (
+//     <div className='min-h-screen bg_gradient'>
+//         <Header />
+//         <LandingPage />
+//         <Footer />
+//     </div>
+//   )
+// }
 
-export default Home
+// export default Home
