40fde85988e29a12c197c4455c515f06acd8becc
ravi <samcollins9899@gmail.com>
2023-09-15 00:11:48 +0530

    added fix 8

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import { Login, Register } from "./components/pages";
 import Home from "./components/Home";
 import { Routes, Route} from "react-router-dom";
 import Profile from "./components/profile";
+import axios from "axios";
+import { useEffect } from "react";
 
+function getUser(){
+  const url= `${import.meta.env.VITE_REACT_APP_API_URL}/auth/login/success`;
+  axios.get(url,{withCredentials:true, headers: {
+    'Access-Control-Allow-Origin': '*', 
+    'Content-Type': 'application/json'
+}
+}).then(data => console.log(data))
 
+}
 
-
-
-
+useEffect(()=>{
+  getUser()
+},[])
 
 function App() {
 
