2778738980ec2fe31f5ce17467b28ecccedcb999
ravi <samcollins9899@gmail.com>
2023-09-15 00:41:33 +0530

    added test fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,37 @@
-
 import { Login, Register } from "./components/pages";
 import Home from "./components/Home";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Profile from "./components/profile";
 import axios from "axios";
 import { useEffect } from "react";
 
-function getUser(){
-  const url= `${import.meta.env.VITE_REACT_APP_API_URL}/auth/login/success`;
-  axios.get(url,{withCredentials:true, headers: {
-    'Access-Control-Allow-Origin': '*', 
-    'Content-Type': 'application/json'
+function getUser() {
+  const url = `${import.meta.env.VITE_REACT_APP_API_URL}/auth/login/success`;
+  axios
+    .get(url, {
+      
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 }
-}).then(data => console.log(data)).catch(error=>console.log(error))
-
-}
-
-
 
 function App() {
-  useEffect(()=>{
-    getUser()
-  },[])
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
-    
-      <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/app" element={<Profile />} />
-        
-        
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/app" element={<Profile />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
 
