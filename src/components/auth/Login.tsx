824edfa20924a365f1fac6a592833aeed08f4b2c
ravi <samcollins9899@gmail.com>
2023-09-20 13:12:40 +0530

    removed unused package

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,6 +1,6 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { Link, json } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 import { useNavigate } from "react-router-dom";
